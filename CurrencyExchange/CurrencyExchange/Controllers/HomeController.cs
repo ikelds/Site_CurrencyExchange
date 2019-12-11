7064bf7f8e1f78c6fb160e40be3fca7d553725fe
ikelds <40229235+ikelds@users.noreply.github.com>
2019-12-12 00:12:52 +0300

    First commit

---
+++ CurrencyExchange/CurrencyExchange/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CurrencyExchange.Models;
+
+namespace CurrencyExchange.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.CursCB = LoadCursCB.LoadCurs();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Основная информация о нас.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Свяжитесь с нами по контактам указанным ниже.";
+
+            return View();
+        }
+    }
+}
---
+++ CurrencyExchange/CurrencyExchange/Models/LoadCursCB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace CurrencyExchange.Models
+{
+    public class LoadCursCB
+    {
+                
+        public static XmlDocument LoadCurs()
+        {
+            XmlDocument xDoc = new XmlDocument();
+
+            xDoc.Load("C:\\Temp\\XML_daily.asp.odm");
+
+            return xDoc;
+        }
+       
+    }
+}
